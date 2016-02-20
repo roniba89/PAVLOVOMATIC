ab2a9a8760b7f6647a96c467223cf96617d0e011
Gal Weinstock <wein88@gmail.com>
2016-02-20 15:39:49 +0200

    fixed bamba dispenser

--- bambaDispenser/bambaDispenser.ino
+++ bambaDispenser/bambaDispenser.ino
@@ -9,17 +9,14 @@
 
 void giveOneTreat(int bamba);
 
-const int STEPS_PER_REVOLUTION = 32;
+const int STEPS_PER_REVOLUTION = 64;
 int treat = 0;
 
 Stepper bambaStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
 
 void setup() {
-  bambaStepper.setSpeed(500);
+  bambaStepper.setSpeed(300);
   Serial.begin(9600);
-
-
-
 }
 
 void loop() {
@@ -36,10 +33,9 @@ void loop() {
 void giveOneTreat(int bamba) {
   if (bamba == 1) {
     Serial.println("One treat");
-    for (int i = 0; i < 64; i++) {
+    for (int i = 0; i < 32; i++) {
       bambaStepper.step(STEPS_PER_REVOLUTION);
-      //bambaStepper.setSpeed(60);
-      //delay(300);
+      delay(3);
     }
   }
 
