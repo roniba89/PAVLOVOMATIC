9b3a1abfd4274aa36452f054524808fbb2d6f127
Gal Weinstock <wein88@gmail.com>
2016-02-20 12:39:06 +0200

    fixed some problems with the motor and added a Serial reader

--- bambaDispenser/bambaDispenser.ino
+++ bambaDispenser/bambaDispenser.ino
@@ -1,21 +1,47 @@
 /*
- * PAVLOVOMATIC
- * by Gal Weinstock, Roni Ben Ari, Ishai Shoham
- * 
- */
+   PAVLOVOMATIC
+
+   Bamba treat dispenser
+
+*/
 
 #include <Stepper.h>
 
-const int STEPS_PER_REVOLUTION = 200;
+void giveOneTreat(int bamba);
+
+const int STEPS_PER_REVOLUTION = 32;
+int treat = 0;
 
 Stepper bambaStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
 
 void setup() {
-  bambaStepper.setSpeed(60);
+  bambaStepper.setSpeed(500);
   Serial.begin(9600);
+
+
+
 }
 
 void loop() {
-  Serial.println("One treat");
-  bambaStepper.step(STEP_PER_REVOLUTION);
+  while (Serial.available() > 0) {
+    treat = Serial.parseInt();
+    if (treat == 1) {
+      giveOneTreat(treat);
+    }
+    treat = 0;
+  }
 }
+
+
+void giveOneTreat(int bamba) {
+  if (bamba == 1) {
+    Serial.println("One treat");
+    for (int i = 0; i < 64; i++) {
+      bambaStepper.step(STEPS_PER_REVOLUTION);
+      //bambaStepper.setSpeed(60);
+      //delay(300);
+    }
+  }
+
+}
+
